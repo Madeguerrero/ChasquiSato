ab72088f745d1648e4701b16f071b9c0984c4ffa
Madeleine <madeleine.guerrero26@gmail.com>
2024-04-30 23:15:00 +0100

    Add files via upload

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,6 +2,6 @@
 // Para cambiar facilmente entre la URL de LOCALHOST y la URL de la base de datos en línea,
 // he creado un CONST llamado apiUrl que puedo usar en varias páginas
 
-export const apiUrl = 'https://restaurantedb-ki3b.onrender.com/';
+export const apiUrl = 'https://chasquisatodb.onrender.com/';
 
 // export const apiUrl = 'http://127.0.0.1:3000/';
