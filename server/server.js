2f1f218c77a657816e9ba61cb91d43b723fb8ecf
Madeleine <madeleine.guerrero26@gmail.com>
2024-05-08 20:02:04 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ try {
 
 app.post("/login", (req, res) => {
   const { email, contrasena } = req.body;
-  const results = [];
+  const result = [];
   db.query(
     "SELECT * FROM clientes WHERE email= ? AND contraseña= ?",
     [email, contrasena],
